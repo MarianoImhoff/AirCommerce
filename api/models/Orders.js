18c1c42f36db8ab40cafad01aea77a01218ec2f2
Santino <santitech445@gmail.com>
2022-06-07 16:28:23 -0300

    Relaciones creadas

--- api/models/Orders.js
+++ api/models/Orders.js
@@ -1,4 +1,20 @@
+const S = require("sequelize")
+const db = require("../db")
 
+class Orders extends S.Model{}
 
+Orders.init({
+    //ver si es asi o con un hook
+orderNumber:{
+    type: S.INTEGER
+},
+userId:{
+    type: S.INTEGER
+},
+productId:{
+    type: S.INTEGER
 
-  
+}
+
+})
+module.exports = Orders
