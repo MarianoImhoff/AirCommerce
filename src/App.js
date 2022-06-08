606c1c6bb23e4f3ea5424c35b30fcc5f8568b471
noeliacossio <noeliacos_8@hotmail.com>
2022-06-08 18:16:09 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
+import React from 'react';
 import { Route, Routes } from 'react-router';
 
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar" 
 import Grid from "./components/Grid"
-import Home from "./components/Home"
+/* import Home from "./components/Home"
 import Account from "./components/Account"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
-import Account from "./components/Account"
 import Users from './components/Users';
 import  Profile  from './components/Profile';
-import  History  from './components/History';
+import  History  from './components/History'; */
 import Cart from "./components/Cart";
-import Checkout from "./components/Checkout";
+/* import Checkout from "./components/Checkout"; */
 import ProductView from "./components/ProductView"
-import NewProduct from "./components/NewProduct"
+/* import NewProduct from "./components/NewProduct" */
 
 
 
@@ -24,22 +24,23 @@ export default function App() {
         <>
         <div>
             <Navbar />
-        </div>
+            
+        </div> 
 
             <Routes>
-                <Route exact path="/" element={<Home />} />
+               {/*  <Route exact path="/" element={<Home />} /> */}
                 <Route exact path="/products" element={<Grid />} />
-                <Route exact path="/login" element={<Login />} />
-                <Route exact path="/signup" element={<Signup />} />
+               {/*  <Route exact path="/login" element={<Login />} /> */}
+               {/*  <Route exact path="/signup" element={<Signup />} />
                 <Route path='/account' element={<Account />} />
                 <Route exact path="/users" element={<Users />} />
                 <Route exact path="/profile" element={<Profile />} />
-                <Route path='/history' element={<History />} />
+                <Route path='/history' element={<History />} /> */}
                 <Route exact path="/cart" element={<Cart />} />
-                <Route exact path="/checkout" element={<Checkout />} />
+               {/*  <Route exact path="/checkout" element={<Checkout />} /> */}
                 <Route exact path="/product_view/:id" element={<ProductView />} />
-                <Route exact path="/new_product" element={<NewProduct />} />
-                <Route path='/update_product/:id' element={<NewProduct />} />
+                {/* <Route exact path="/new_product" element={<NewProduct />} />
+                <Route path='/update_product/:id' element={<NewProduct />} /> */}
             </Routes>
         </>
 
