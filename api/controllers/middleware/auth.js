1edae95e859adc72cce07b6b66de6d93c5701fc7
Santino <santitech445@gmail.com>
2022-06-08 18:17:03 -0300

    CRUD Finalizado, rutas del admin finalizada

---
+++ api/controllers/middleware/auth.js
@@ -0,0 +1,24 @@
+const Users = require('../../models/Users');
+
+// Verificacion de que el usuario este logeado en AirCommerce
+
+const Auth = (req, res, next) => {
+  if (!Users) return res.status(401).send('Unauthorized,Loggeate primero');
+  next();
+};
+
+// Verificacion de que el usuario sea SuperAdmin en AirCommerce
+
+const AuthAdmin = (req, res, next) => {
+  Users.findOne({ where: { id: req.params.id } }).then((admin) => {
+    console.log(admin.dataValues.superAdmin);
+    if (admin.dataValues.superAdmin !== 'true')
+      return res
+        .status(401)
+        .send('Unauthorized, No tenes los permisos necesarios ');
+  });
+
+  next();
+};
+
+module.exports = { Auth, AuthAdmin };
