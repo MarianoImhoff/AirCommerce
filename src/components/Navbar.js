868717e141356957cbcf8c2a24d30c1fa9b8b413
Matias Jaliff <matiasjaliff@gmail.com>
2022-06-09 18:25:51 -0300

    cambios 08-jun

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,71 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container, Navbar, NavDropdown, Button } from 'react-bootstrap';
 import { FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import logo from "../utils/img/Logo.jpg";
+import axios from 'axios';
+import logo from '../utils/img/Logo.jpg';
 import Search from './Search';
 
+import { AuthContext } from '../context/AuthContext';
+
 const NavBar = () => {
-    return (
-        <Navbar bg="dark" expand="md">
-            <Container fluid>
-                    <Link style={{paddingLeft: 13, textDecoration: 'none'}} to="/">
-                        <img
-                            src={logo}
-                            width="80"
-                            height="80"
-                            style={{'borderRadius':'5px'}}
-                            alt="logo"
-                        /> 
-                        <h1>AirCommerce</h1>
-                    </Link>
-                    <Link style={{ textDecoration: 'none' }} to="/Products">Tienda</Link>
+  const { user, toggleAuth } = useContext(AuthContext);
 
-                    <NavDropdown title="Filter" >
-                        <NavDropdown.Item href="">Talla</NavDropdown.Item>
-                        <NavDropdown.Item href="">Marca</NavDropdown.Item>
-                        <NavDropdown.Item href="">Precio</NavDropdown.Item>
-                    </NavDropdown>
+  const handleLogout = () => {
+    console.log('TRYING LOGOUT...');
+    axios
+      .post('/users/logout')
+      .then((res) => {
+        console.log(res);
+        toggleAuth(null);
+        console.log('LOGOUT DONE');
+      })
+      .catch((err) => console.log('ERROR: ', err));
+  };
 
-                    <Search />
+  return (
+    <Navbar bg="dark" expand="md">
+      <Container fluid>
+        <Link style={{ paddingLeft: 13, textDecoration: 'none' }} to="/">
+          <img
+            src={logo}
+            width="80"
+            height="80"
+            style={{ borderRadius: '5px' }}
+            alt="logo"
+          />
+          <h1>AirCommerce</h1>
+        </Link>
+        <Link style={{ textDecoration: 'none' }} to="/Products">
+          Tienda
+        </Link>
 
-                    <Link to="/Login">
-                        <Button variant="outline-success">Login</Button>
-                    </Link>
-                    <Link to="/Cart">
-                        <Button variant="outline-success"><FaShoppingCart /></Button>
-                    </Link>
-               
-            </Container>
-        </Navbar>
-    )
-}
+        <NavDropdown title="Filter">
+          <NavDropdown.Item href="">Talla</NavDropdown.Item>
+          <NavDropdown.Item href="">Marca</NavDropdown.Item>
+          <NavDropdown.Item href="">Precio</NavDropdown.Item>
+        </NavDropdown>
 
-export default NavBar
+        <Search />
+
+        {user ? (
+          <Button onClick={handleLogout} variant="outline-success">
+            Logout
+          </Button>
+        ) : (
+          <Link to="/Login">
+            <Button variant="outline-success">Login</Button>
+          </Link>
+        )}
+
+        <Link to="/Cart">
+          <Button variant="outline-success">
+            <FaShoppingCart />
+          </Button>
+        </Link>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
